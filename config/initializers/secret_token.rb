845ad843d2eea6dfb1546a0ea28daa601ade056f
Fai Alqadi <faisal.alqadi@gmail.com>
2014-09-16 15:50:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitter::Application.config.secret_key_base = '71479e6a668aecac9975f1da95cf7846c19791d10f8b4745415825ef6fb8bc5cb42f829663fed373d10254fa68602756d300510788dde5c3ba0caa38a7d1e3ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bitter::Application.config.session_store :cookie_store, key: '_bitter_session'
